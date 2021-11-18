7d849641f5879e8936de0aeccce1e31f209fb61e
eremenko.v.o <v.eremenko@isoit.ru>
2021-11-18 18:07:44 +0300

    fix url for get response

--- index.js
+++ index.js
@@ -8,29 +8,32 @@ bot.start((ctx) => {
   ctx.reply(
     'Добро пожаловать. Этот бот для уведомления о погоде на сегодня. Бот находиться в разработке, так что учитывайте это 😉',
   );
-  ctx.reply('Для начала пришлите геопозицию');
+  ctx.reply(
+    'Для начала пришлите геопозицию или напишите название населенного пункта',
+  );
 });
 
 bot.help((ctx) => {
-  ctx.reply('Напишите название города и я покажу вам погоду');
+  ctx.reply('Пришлите геопозицию или напишите название населенного пункта');
 });
 
 bot.on('message', (ctx) => {
-  if (ctx.message?.location?.latitude) {
-    axios
-      .get(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${ctx.message.location.latitude}&lon=${ctx.message.location.longitude}&appid=${process.env.API_KEY}`,
-      )
-      .then(function (response) {
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        console.error(error);
-        ctx.reply('Ошибка получения ответа от сервера');
-      });
-  } else {
-    ctx.reply('Пришлите свою геопозицию');
-  }
+  let url = ctx.message.location
+    ? `https://api.openweathermap.org/data/2.5/weather?lat=${ctx.message.location.latitude}&lon=${ctx.message.location.longitude}&appid=${process.env.API_KEY}`
+    : `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(
+        ctx.message.text,
+      )}&appid=${process.env.API_KEY}`;
+
+  console.log(url);
+  axios
+    .get(url)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+      ctx.reply('Ошибка получения ответа от сервера');
+    });
 });
 
 bot.launch();
